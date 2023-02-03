7246c571dc28a9d255145b96179faae93f48097b
Daniel Fontaine <fontaine@sfsu.edu>
2023-02-03 01:01:21 -0800

    first

--- resource_nodes/modules.py
+++ resource_nodes/modules.py
@@ -13,14 +13,28 @@ load_dotenv(r"C:\Users\DanielPC\Desktop\Servers\canvas-bot-v2\network\.env")
 
 class Modules:
 
-    def __init__(self, course_id):
+    def __init__(self, course_id, parent):
+        self.parent = parent
+        self.children = []
         self.course_id = course_id
         self.api_url = f"{os.environ.get('api_path')}/{self.course_id}/modules?access_token={os.environ.get('access_token')}"
         self.api_request_content = None
-
+        self.get_modules_list()
+        self.sort_children()
 
     def get_modules_list(self):
-        self.api_request_content = json.loads(requests.get(test.api_url).content)
+        self.api_request_content = json.loads(requests.get(self.api_url).content)
+
+
+    def sort_children(self):
+        for each in self.api_request_content:
+            print(each['items_url'])
+            string = f"{each['items_url']}?access_token={os.environ.get('access_token')}?per_page=20"
+            print(string)
+            items = requests.get(f"{each['items_url']}?access_token={os.environ.get('access_token')}&per_page=100")
+            # print(json.loads(items.content))
+            for item in json.loads(items.content):
+                print(item)
 
 
 
@@ -35,9 +49,4 @@ class Modules:
 
 
 
-
-
-test = Modules('18411')
-test.get_modules_list()
-
-print(test.api_request_content)
+test = Modules('18411', "fgef")
