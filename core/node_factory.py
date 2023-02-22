bb195fea85d9f1d2e2880d0eb375cfe86d0d361f
fontaineconsult@gmail.com <!Babybops!1>
2023-02-21 17:02:06 -0800

    getting there

--- core/node_factory.py
+++ core/node_factory.py
@@ -2,11 +2,12 @@
 from typing import Union, Type
 from resource_nodes.assignments import Assignment
 from resource_nodes.announcements import Announcement
+from resource_nodes.canvasfiles import CanvasFile
 from resource_nodes.discussions import Discussion
 from resource_nodes.modules import Module
 from resource_nodes.pages import Page
 from resource_nodes.quizzes import Quiz
-
+import re
 from network.api import *
 
 
@@ -31,6 +32,35 @@ def get_node(type) -> Union[Type[Assignment],
     }
 
     return node_dict.get(type)
+
+
+
+def get_node_by_a_tag_match(a_tag) -> Union[Type[Assignment],
+                            Type[Page],
+                            Type[Quiz],
+                            Type[Announcement],
+                            Type[Module],
+                            Type[Discussion],
+                            Type[CanvasFile],
+                            None]:
+
+
+
+
+
+
+    node_dict = {
+        "assignment": Assignment,
+        "announcement": Announcement,
+        "discussion": Discussion,
+        "module": Module,
+        "page": Page,
+        "quiz": Quiz,
+        "file": CanvasFile,
+    }
+
+    return node_dict.get(type)
+
 
 
 def get_endpoint(node):
