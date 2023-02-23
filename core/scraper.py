dd17179a8aa3306621ad8bb4140bb0adb576bd4f
Daniel Fontaine <fontaine@sfsu.edu>
2023-02-23 00:24:11 -0800

    third

--- core/scraper.py
+++ core/scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from sorters.sorters import resource_node_regex
 
 
-def get_general_links_from_html_a_tag(html_body):
+def get_href_links_from_html_a_tag(html_body):
 
     if html_body is None:
         return list()
@@ -13,8 +13,7 @@ def get_general_links_from_html_a_tag(html_body):
                 if a_tag.get('href') is not None and resource_node_regex.search(a_tag.get('href')) is None]
 
 
-
-def get_general_links_from_html_iframe_tag(html_body):
+def get_src_links_from_html_iframe_tag(html_body):
 
     if html_body is None:
         return list()
@@ -24,9 +23,28 @@ def get_general_links_from_html_iframe_tag(html_body):
                 if a_tag.get('src') is not None and resource_node_regex.search(a_tag.get('src')) is None]
 
 
+def get_src_links_from_img_tag(html_body):
+
+    if html_body is None:
+        return list()
+    soup = BeautifulSoup(html_body)
+    if soup:
+        return [(a_tag.get('src'), a_tag.text.strip()) for a_tag in soup.find_all('img')
+                if a_tag.get('src') is not None and resource_node_regex.search(a_tag.get('src')) is None]
+
+
+def get_src_links_from_video_tag(html_body):
+
+    if html_body is None:
+        return list()
+    soup = BeautifulSoup(html_body)
+    if soup:
+        return [(a_tag.get('src'), a_tag.text.strip()) for a_tag in soup.find_all('video')
+                if a_tag.get('src') is not None and resource_node_regex.search(a_tag.get('src')) is None]
 
 
 def get_data_api_links_from_html(html_body):
     soup = BeautifulSoup(html_body)
     if soup:
-        return [(a_tag.get('data-api-endpoint'), a_tag.text.strip()) for a_tag in soup.find_all('a') if a_tag.get('data-api-endpoint') is not None]
+        return [(a_tag.get('data-api-endpoint'), a_tag.text.strip())
+                for a_tag in soup.find_all('a') if a_tag.get('data-api-endpoint') is not None]
---
+++ resource_nodes/content_nodes.py
@@ -0,0 +1,45 @@
+from resource_nodes.base_content_node import BaseContentNode
+
+
+class Document(BaseContentNode):
+
+    def __init__(self):
+        super().__init__()
+
+
+
+class DocumentSite(BaseContentNode):
+
+    def __init__(self):
+        super().__init__()
+
+
+class VideoSite(BaseContentNode):
+
+
+    def __init__(self):
+        super().__init__()
+
+
+class VideoFile(BaseContentNode):
+
+    def __init__(self):
+        super().__init__()
+
+
+class AudioFile(BaseContentNode):
+
+    def __init__(self):
+        super().__init__()
+
+
+class AudioSite(BaseContentNode):
+
+    def __init__(self):
+        super().__init__()
+
+
+class Unsorted(BaseContentNode):
+
+    def __init__(self):
+        super().__init__()
