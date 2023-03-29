74741a13b158548ad8dd8d4c7bcc2281772bdbbd
Daniel Fontaine <fontaine@sfsu.edu>
2023-03-28 23:52:57 -0700

    testing file path length reducer

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 windowsvenv\Scripts\activate && ^
 pyinstaller --onefile --name canvas_bot --console --add-data "config\config.yaml;config\." ^
 --add-data "config\download_manifest.yaml;config\." --add-data "config\re.yaml;config\." ^
---paths "C:\Users\DanielPC\Desktop\Servers\canvas-bot-v2" ^
---paths "C:\Users\DanielPC\Desktop\Servers\canvas-bot-v2\config" canvas_bot.py
+--paths "%~dp0" ^
+--paths "%~dp0config" canvas_bot.py
