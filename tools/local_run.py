8bb6c7ce42e014ca0d28220e2568d3e224f4b4dc
Daniel Fontaine <fontaine@sfsu.edu>
2025-10-08 12:06:52 -0700

    localruns

--- tools/local_run.py
+++ tools/local_run.py
@@ -15,10 +15,10 @@ cpage = [
 
 
 
-bot = CanvasBot(str(57924))
+bot = CanvasBot(str(59736))
 
 
 bot.start()
 bot.print_content_tree()
-bot.save_content_as_json(json_save_directory=r"C:\Users\913678186\IdeaProjects\canvas-bot-v2",
-                         file_download_directory=r"C:\Users\913678186\IdeaProjects\canvas-bot-v2")
+# bot.save_content_as_json(json_save_directory=r"C:\Users\913678186\IdeaProjects\canvas-bot-v2",
+#                          file_download_directory=r"C:\Users\913678186\IdeaProjects\canvas-bot-v2")
